19ee520b08f110699bae556e36b52755c0ccfbf9
Asad-Nawaz92 <asadn7862@gmail.com>
2024-06-01 15:08:11 +0500

    Modified

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
+// SIGN UP AND SIGN IN
+
 function signup() {
   var nameInput = document.getElementById("signup-name");
   var emailInput = document.getElementById("signup-email");
   var passwordInput = document.getElementById("signup-pass");
   var confirmpasswordInput = document.getElementById("signup-confirm-pass");
+  var agreeCheckbox = document.getElementById("agree");
 
   var name = nameInput.value;
   var email = emailInput.value;
   var password = passwordInput.value;
   var confirmPassword = confirmpasswordInput.value;
+  var agreeChecked = agreeCheckbox.checked;
 
-  var newUser = {
-    email: email,
-    password: password,
-  };
-
-  users.push(newUser);
-  localStorage.setItem("users", JSON.stringify(users));
-  location.href = "./signin.html";
-
-  if (password != confirmPassword) {
-    alert("Passwords do not match.");
-    return;
-  }
+  var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   if (
     name === "" ||
@@ -29,14 +21,50 @@ function signup() {
     email === "" ||
     password === ""
   ) {
-    alert("Please fill in all fields.");
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Please fill in all fields.",
+    });
+    return;
+  }
+
+  if (!agreeChecked) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Please agree to the terms and conditions.",
+    });
+    return;
+  }
+
+  if (!emailPattern.test(email)) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Invalid email format. Please use the format: example@gmail.com.",
+    });
     return;
   }
 
   if (password.length < 6) {
-    alert("Password must be at least 6 characters long.");
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Password must be at least 6 characters long.",
+    });
     return;
   }
+
+  if (password !== confirmPassword) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Passwords do not match.",
+    });
+    return;
+  }
+
   var usersJSON = localStorage.getItem("users");
   var users = usersJSON ? JSON.parse(usersJSON) : [];
 
@@ -45,17 +73,53 @@ function signup() {
   });
 
   if (existingUser) {
-    alert("User with this email already exists. Please sign in.");
-    location.href = "./signin.html";
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "User with this email already exists. Please Login.",
+    }).then(() => {
+      location.href = "./signin.html";
+    });
     return;
   }
+
+  var newUser = {
+    name: name,
+    email: email,
+    password: password,
+  };
+
+  users.push(newUser);
+  localStorage.setItem("users", JSON.stringify(users));
+  location.href = "./signin.html";
 }
 
 function signin() {
   var emailInput = document.getElementById("signin-email");
   var passwordInput = document.getElementById("signin-pass");
+
   var email = emailInput.value;
   var password = passwordInput.value;
+
+  if (email === "" || password === "" || !rememberChecked) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Please fill in all fields.",
+    });
+    return;
+  }
+
+  var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  if (!emailPattern.test(email)) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Invalid email format. Please use the format: example@gmail.com.",
+    });
+    return;
+  }
 
   var usersJSON = localStorage.getItem("users");
   var users = usersJSON ? JSON.parse(usersJSON) : [];
@@ -67,7 +131,26 @@ function signin() {
   if (user) {
     location.href = "./welcome.html";
   } else {
-    alert("Invalid email or password. Please try again.");
-    location.href = "./signin.html";
+    if (
+      !users.some(function (e) {
+        return e.email === email;
+      })
+    ) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Invalid email. Please try again.",
+      });
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Invalid password. Please try again.",
+      });
+    }
   }
+}
+
+function logOut() {
+  location.href = "./signin.html";
 }
